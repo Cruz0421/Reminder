c1fdd487dd1587f03bda3e1868349d7b904679c0
Patrick Cruz <83104613+Cruz0421@users.noreply.github.com>
2023-05-03 21:00:33 -0700

    update code

--- RemindApp(VSCode)/Reminder/login.php
+++ RemindApp(VSCode)/Reminder/login.php
@@ -1,16 +1,42 @@
 <?php
+// DEBUGGING:
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require 'connect.php';
+
+session_start();
+
 if(isset($_POST['email']) && isset($_POST['password'])){
-    require_once "connect.php";
-    require_once "validate.php";
-    $email = validate($_POST['email']);
-    $password = validate($_POST['password']);
-    $sql = "select * from users where email= '$email' and password='" . md5($password) . "'";
-    $result = $conn->query($sql);
+    $unsanitized_username = $_POST['username'];
+    $unsanitized_pwd = $_POST['pwd'];
+
+    // sanitize the data
+    $username = filter_var($unsanitized_username, FILTER_SANITIZE_STRING);
+    $pwd = filter_var($unsanitized_pwd, FILTER_SANITIZE_STRING);
     
-    if($result->num_rows > 0){
-        echo "success";
-    }else{
-        echo "fail";
+    // validate that username exists in database
+    // - use prepare statements to prevent sql injections
+    $query = "SELECT pwd FROM `user` WHERE `username` = :username";
+    $stmt = $db->prepare($query);
+    $stmt->bindParam(':username', $username);
+
+    // get results
+    $result = $stmt->execute();
+    $row = $result->fetchArray(SQLITE3_ASSOC);
+    if($row)
+    {
+        // now check that the hashed password is correct
+        if (password_verify($pwd, $row['pwd']))
+        {
+            $_SESSION['loggedin'] = true;
+            $_SESSION['username'] = $username;
+        }
+    }
+    else
+    {
+        $_SESSION['loggedin'] = false;
     }
 }
 ?>
