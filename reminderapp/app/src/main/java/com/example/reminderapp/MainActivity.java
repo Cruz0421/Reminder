7264e6524a33c7b09d8ede770eb9e3b1f4931983
Patrick Cruz <83104613+Cruz0421@users.noreply.github.com>
2023-05-13 22:14:26 -0700

    Login testing branch

--- reminderapp/app/src/main/java/com/example/reminderapp/MainActivity.java
+++ reminderapp/app/src/main/java/com/example/reminderapp/MainActivity.java
@@ -6,171 +6,72 @@ Purpose: Login page for app
 
 package com.example.reminderapp;
 
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
+public class MainActivity extends AppCompatActivity {
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+    EditText username, password, repassword;
+    Button signup, login;
+    DBHelper DB;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    private String URL = "ssh://artemis_csub/home/stu/pcruz/public_html/Reminder/login.php";
-
-    Button loginbutton;
-    Button registerbutton;
-    TextView message;
-    TextView errortext;
-    EditText etEmail;
-    EditText etPassword;
-
-    String errorMsg = "";
-    String threadString = "";
-    String email = "";
-    String password = "";
-
-    database dbHandler;
-
-    // main functionality
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dbHandler = new database(this);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        repassword = (EditText) findViewById(R.id.repassword);
+        signup = (Button) findViewById(R.id.btnsignup);
+        login = (Button) findViewById(R.id.btnlogin);
+        DB = new DBHelper(this);
 
-        loginbutton = (Button) findViewById(R.id.loginbutton);
-        loginbutton.setOnClickListener(this);
-        registerbutton = (Button) findViewById(R.id.registerbutton);
-        registerbutton.setOnClickListener(this);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                String repass = repassword.getText().toString();
 
-        message = (TextView) findViewById(R.id.logintext);
-        errortext = findViewById(R.id.errortext);
-        errortext.setText(errorMsg);
-
-        etEmail = findViewById(R.id.inputEmail);
-        etPassword = findViewById(R.id.inputPass);
-
-        // start thread
-        Thread thread = new Thread(new Thx());
-        thread.start();
-
-    }
-
-    // creates background service to send notifications even when app is off
-    @Override
-    protected void onStop() {
-        super.onStop();
-        startService(new Intent(this, NotificationService.class));
-    }
-
-    // TODO: implement login
-    public void login () {
-        email = etEmail.getText().toString().trim();
-        password = etPassword.getText().toString().trim();
-        final Intent success = new Intent(this, ReminderList.class);
-
-        if(!email.equals("") && !password.equals("")) {
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    if (response.equals("success")) {
-                        startActivity(success);
-                        finish();
-                    } else if (response.equals("failure")) {
-                        errorMsg = "Incorrect email and/or password.";
-                        errortext.setText(errorMsg);
-                        //Toast.makeText(MainActivity.this, "Invalid Login Id/Password", Toast.LENGTH_SHORT).show();
+                if(user.equals("")||pass.equals("")||repass.equals(""))
+                    Toast.makeText(MainActivity.this, "Please Fill all blanks", Toast.LENGTH_SHORT).show();
+                else{
+                    if(pass.equals(repass)){
+                        Boolean checkuser = DB.chechusername(user);
+                        if(checkuser==false){
+                            Boolean insert = DB.insertData(user, pass);
+                            if(insert==true) {
+                                Toast.makeText(MainActivity.this, "Registered Success", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), NotificationService.class);
+                                startActivity(intent);
+                            }else{
+                                Toast.makeText(MainActivity.this, "Registered Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }else{
+                            Toast.makeText(MainActivity.this, "Username Exists! Sign in", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(MainActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
                     }
                 }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(MainActivity.this, error.toString().trim(), Toast.LENGTH_SHORT).show();
-                }
-            }){
-                @Nullable
-                @Override
-                protected Map<String, String> getParams() throws AuthFailureError {
-                    Map<String, String> data = new HashMap<>();
-                    data.put( "email", email);
-                    data.put("password", password);
-                    return data;
-                }
-            };
-            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
-            requestQueue.add(stringRequest);
-        }else{
-            errorMsg = "Please input email and password.";
-            errortext.setText(errorMsg);
-            //Toast.makeText(this, "Fields can not be empty!", Toast.LENGTH_SHORT).show();
-        }
-    }
 
-    public void register(View view) {
-        final Intent register = new Intent(MainActivity.this, Register.class);
-        startActivity(register);
-        finish();
-    }
-
-    // handler for click events
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.loginbutton:
-                //what should happen when user clicks login
-                //login();
-                final Intent temp = new Intent(this, ReminderList.class);
-                startActivity(temp);
-                break;
-            case R.id.registerbutton:
-                // what should happen when user clicks register
-                final Intent temp2 = new Intent(this, Register.class);
-                startActivity(temp2);
-                break;
-        }
-    }
-
-    // thread that connects to file online
-    public class Thx implements Runnable {
-        @Override
-        public void run() {
-            try {
-                java.net.URL url = new URL("https://cs.csub.edu/~thooser/3350/apptext.txt");
-                BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-                StringBuilder total = new StringBuilder();
-                String line;
-                while ((line = in.readLine()) != null) {
-                    total.append(line + " ");
-                }
-                threadString = total.toString();
-                in.close();
-            } catch (MalformedURLException e) {
-            } catch (IOException e) {
             }
-            return;
-        }
-        //});
-        //thread.start();
-    };
+        });
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
 }
